782e85806985c7dcb7640781387b78756c804e8c
Andy Bond <andybond87@gmail.com>
2017-02-15 08:00:10 +0000

    Added bootstrap style file upload field

--- transactions/forms.py
+++ transactions/forms.py
@@ -7,19 +7,20 @@ class OFX_Form(forms.ModelForm):
         fields = ('file_location', ) #Uses file_location field from the Uploaded_File model
         labels = {'file_location': 'Choose OFX file'}
 
+
 class TX_Add(forms.ModelForm):
     class Meta:
         model = Transaction
         fields = ('account', 'tx_type', 'date', 'description', 'amount')
         labels = {'tx_type': 'Type'}
-        widgets = {'date': forms.TextInput(attrs={'class':'datepicker'})}
+        widgets = {'date': forms.TextInput(attrs={'class':'datepicker','autocomplete':'off'})}
 
 class TX_History(forms.Form):
-    start_date = forms.DateField(label="Period start", widget=forms.TextInput(attrs={'class':'datepicker'}))
-    end_date = forms.DateField(label="Period end", widget=forms.TextInput(attrs={'class':'datepicker'}))
+    start_date = forms.DateField(label_suffix="",label="Period start", widget=forms.TextInput(attrs={'class':'datepicker','autocomplete':'off'}))
+    end_date = forms.DateField(label_suffix="",label="Period end", widget=forms.TextInput(attrs={'class':'datepicker','autocomplete':'off'}))
     #Number of account fields varies, so have to loop and directly add to parent's self.fields attribute
     def __init__(self, *args, **kwargs):
         super(TX_History, self).__init__(*args, **kwargs)
         for account in Account.objects.all():
             #Have to convert primary key to string to use as field name, otherwise field always returns 'false'
-            self.fields[str(account.pk)] = forms.BooleanField(label=account.description, required=False, initial=True)
+            self.fields[str(account.pk)] = forms.BooleanField(label_suffix="",label=account.description, required=False, initial=True)
--- transactions/static/transactions/jquery-ui-1.12.1.custom/external/jquery/jquery.js
+++ transactions/static/transactions/jquery-ui-1.12.1.custom/external/jquery/jquery.js
@@ -12,6 +12,39 @@
  * Date: 2016-05-20T17:17Z
  */
 
+/*!
+*Extra added from here: https://www.abeautifulsite.net/whipping-file-inputs-into-shape-with-bootstrap-3
+*/
+ $(function() {
+
+   // We can attach the `fileselect` event to all file inputs on the page
+   $(document).on('change', ':file', function() {
+     var input = $(this),
+         numFiles = input.get(0).files ? input.get(0).files.length : 1,
+         label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+     input.trigger('fileselect', [numFiles, label]);
+   });
+
+   // We can watch for our custom `fileselect` event like this
+   $(document).ready( function() {
+       $(':file').on('fileselect', function(event, numFiles, label) {
+
+           var input = $(this).parents('.input-group').find(':text'),
+               log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+           if( input.length ) {
+               input.val(log);
+           } else {
+               if( log ) alert(log);
+           }
+
+       });
+   });
+
+ });
+
+
+
 (function( global, factory ) {
 
 	if ( typeof module === "object" && typeof module.exports === "object" ) {
